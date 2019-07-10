08ce629370b76ec6eb0d36c5c4b6e06f392008bc
nwnn <nwnn@users.noreply.github.com>
2019-07-10 15:09:58 +0900

    moment 日付差分取得処理のサンプル記述

--- server/database.ts
+++ server/database.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document } from 'mongoose';
 import { Schema } from 'mongoose';
+import moment from 'moment';
 
 interface UserDocument extends Document {
     name : string,
@@ -9,6 +10,8 @@ interface UserDocument extends Document {
 interface TaskDocument extends Document {
     name : string,
     user : string,
+    createTime : moment.Moment,
+    dueTime : moment.Moment,
     // owner : string,
 
 }
@@ -19,13 +22,15 @@ mongoose.connect('mongodb://localhost:27017/todolist', {
 });
 
 const User = new Schema({
-    name : String,
+    name : Schema.Types.String,
         
 });
 
 const Task = new Schema({
     name : Schema.Types.String,
-    user : Schema.Types.ObjectId
+    user : Schema.Types.ObjectId,
+    dueTime : Schema.Types.Date,
+    createTime : Schema.Types.Date
     
 });
 
@@ -33,23 +38,24 @@ const UserModel = mongoose.model<UserDocument>('User', User);
 const TaskModel = mongoose.model<TaskDocument>('Task', Task);
 
 (async () => {
-    // const takashi = await UserModel.findById("5d256b8697040f0cfc0f1de9");
-    // console.log(takashi);
-    // if(takashi !== null) {
-    //     const task = new TaskModel();
-    //     task.name = "野菜を買いに行く";
-    //     task.user = takashi._id;
+    const takashi = await UserModel.findById("5d256b8697040f0cfc0f1de9");
+    console.log(takashi);
+    if(takashi !== null) {
+        const task = new TaskModel();
+        task.name = "野菜を買いに行く";
+        task.user = takashi._id;
+        task.createTime = moment();
+        task.dueTime = moment().add(30,"second");
 
-    //     const taskDocument = await task.save();
-    //     takashi.tasks.push(taskDocument._id);
+        const taskDocument = await task.save();
 
-    //     await takashi.save();
-    //     console.log("完了！")
+        await takashi.save();
+        console.log("完了！")
 
-    // } else {
-    //     throw new Error("いない");
+    } else {
+        throw new Error("いない");
 
-    // }
+    }
 
     // const takashi = await UserModel.findById("5d256b8697040f0cfc0f1de9");
 
@@ -57,11 +63,17 @@ const TaskModel = mongoose.model<TaskDocument>('Task', Task);
         from: 'tasks',
         localField: '_id',
         foreignField: 'user',
-        as: 'taskList',
+        as: 'tasks',
     }).match({ _id : mongoose.Types.ObjectId('5d256b8697040f0cfc0f1de9') });
 
     
+    const tks = value[0];
+    for (const task of tks.tasks) {
+        if(task.dueTime !== undefined){
+            console.log(task.name);
+            console.log(moment().diff(task.dueTime));
 
-    console.log(value);
+        }
+    }
 
 })();
