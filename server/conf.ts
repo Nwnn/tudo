970d6f3aff527c60b8412e1633483a9bb7fe0a51
nwnn <nwnn@users.noreply.github.com>
2019-10-14 10:52:38 +0000

    greenlock-express導入、httpsサーバ化

--- server/conf.ts
+++ server/conf.ts
@@ -1,1 +1,2 @@
 export const MONGO_HOST = 'mongodb://tudo-mongo:27017/todoDatabase';
+export const EMAIL = 'example@example.com'
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { Nuxt, Builder } from 'nuxt';
 import nuxtConfig from '../nuxt.config';
 import { api } from './api';
+import greenlockExpress from 'greenlock-express';
+import { EMAIL } from './conf'
 
 const app = express();
 app.use('/api', api);
@@ -24,9 +26,18 @@ nuxtConfig.dev = !(process.env.NODE_ENV === 'production');
 
     app.use(nuxt.render);
 
-    app.listen(port, () => {
-        console.log(`Server listening on http://${host}:${port}`);
-    
-    });
+    greenlockExpress.create({
+        email: EMAIL, // The email address of the ACME user / hosting provider
+        agreeTos: true, // You must accept the ToS as the host which handles the certs
+        configDir: "~/.config/acme/", // Writable directory where certs will be saved
+        communityMember: true, // Join the community to get notified of important updates
+        telemetry: true, // Contribute telemetry data to the project
+ 
+        // Using your express app:
+        // simply export it as-is, then include it here
+        app: app
+ 
+        //, debug: true
+    }).listen(8080,4433)
 
 })();
