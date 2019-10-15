200158459762d3fad6593e28fc56b2302a3f6452
nwnn <nwnn@users.noreply.github.com>
2019-10-15 07:41:53 +0000

    タスク確認のサンプル実装

--- server/googlehome.ts
+++ server/googlehome.ts
@@ -1,10 +1,22 @@
 // Import the appropriate service and chosen wrappers
-import { dialogflow, Image } from "actions-on-google"
-  
+import { dialogflow } from "actions-on-google"
+import { TodoApp } from './todoapp'
+import moment from 'moment';
+
+moment.locale('ja')
+
 // Create an app instance
 export const app = dialogflow()
   
 // Intent in Dialogflow called `Goodbye`
-app.intent('Show Task Intent', (conv) => {
-    conv.close('お前ぶち殺すぞ')
+app.intent('Show Task Intent', async (conv) => {
+    const userTasks = await TodoApp.Tasks.getTasksByUsername("user115")
+    if(userTasks[0] != undefined){ 
+        conv.close(`最新の予定は${ userTasks[0].title }です。締め切りは、${ moment(userTasks[0].dueTime).fromNow() }です。`)
+
+    } else {
+        conv.close('予定はありません')
+
+    }
+
 });
